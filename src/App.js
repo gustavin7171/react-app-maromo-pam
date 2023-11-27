4c3739ad707b50b8fe81e9fb0c16e35af23fb93b
gustavin7171 <gustavinnhs@gmail.com>
2023-11-27 13:54:37 -0300

    projeto com react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+import HomePage from "./components/HomePage";
+function App(){
+  return(
+    <div className="App">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+function Card(){
+    return(
+        <div className="card-container">
+            <p>Componentes</p>
+            <p>Facilita manter partes menores funcionando</p>
+            <p>Você pode reutilizá-los, ou seja, reaproveitamento</p>
+        </div>
+    );
+}
+export default Card;
